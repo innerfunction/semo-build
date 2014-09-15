21ca6372de875a37c703f6a0bc440dd052ac3cfc
Julian Goacher <julian.goacher@innerfunction.com>
2014-09-15 19:50:30 +0100

    Test/debug of in-template image refs.

--- test/test6.js
+++ test/test6.js
@@ -3,7 +3,7 @@ require('../lib/cx')
     var img = cx.image('home-banner.png').write();
     var data = {
         title: 'The Test',
-        banner: img.get('home-banner.png')
+        banner: img.get('home-banner')
     }
     cx.eval('template.html', data ).write('result.html');
 });
