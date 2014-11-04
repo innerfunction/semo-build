b8dc600db5b4e7c71f075f1c9aedf560ad76fd27
Julian Goacher <julian.goacher@innerfunction.com>
2014-11-04 12:14:49 +0000

    Moved main entry scripts (semob, server.js) to project root.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var Log = require('log4js').getLogger('semo-build.server');
-var start = require('./start');
+var start = require('./lib/start');
 // Start the build system with the full set of components. The installer and publisher
 // components are perpetual processes, so the server will continue running once started.
 var components = {
-    install: require('./installer'),    // Manages installation of feed configurations.
-    download: require('./downloader'),  // Manages performing of feed downloads.
-    build: require('./builder'),        // Manages performing of feed builds.
-    publish: require('./publisher')     // Manages publishing of feed builds.
+    install: require('./lib/installer'),    // Manages installation of feed configurations.
+    download: require('./lib/downloader'),  // Manages performing of feed downloads.
+    build: require('./lib/builder'),        // Manages performing of feed builds.
+    publish: require('./lib/publisher')     // Manages publishing of feed builds.
 }
 start( components )
 .then(function() {
