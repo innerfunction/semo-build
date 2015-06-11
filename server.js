293fe9204e7bf788bb81a636bda3197e6572ea74
Julian Goacher <julian.goacher@innerfunction.com>
2015-06-11 13:27:47 +0100

    Changes to support new build system.

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
 var log4js = require('log4js');
 
-var mode;
-process.argv.slice( 2 ).forEach(function( arg ) {
-    switch( arg ) {
-    case '-logconfig':
-    case '-loglevel':
-        mode = arg;
-        break;
-    default:
-        switch( mode ) {
-        case '-logconfig':
-            log4js.configure( arg );
-            break;
-        case '-loglevel':
-            log4js.setGlobalLogLevel( arg );
-            break;
-        }
-    }
-});
-
 var Log = log4js.getLogger('semo-build.server');
 var start = require('./lib/start');
 // Start the build system with the full set of components. The installer and publisher
@@ -30,6 +11,37 @@ var components = {
     build:      require('./lib/builder'),       // Manages performing of feed builds.
     publish:    require('./lib/publisher')      // Manages publishing of feed builds.
 }
+
+var mode;
+process.argv.slice( 2 ).forEach(function( arg ) {
+    switch( arg ) {
+    case '-builder':
+    case '-logconfig':
+    case '-loglevel':
+        mode = arg;
+        break;
+    case '-nopublish':
+        Log.info('Disabling publish component');
+        delete components.publish;
+        break;
+    default:
+        switch( mode ) {
+        case '-builder':
+            if( arg == 'v2' ) {
+                Log.info('Using builder v2');
+                components.build = require('./lib/builder.v2');
+            }
+            break;
+        case '-logconfig':
+            log4js.configure( arg );
+            break;
+        case '-loglevel':
+            log4js.setGlobalLogLevel( arg );
+            break;
+        }
+    }
+});
+
 start( components )
 .then(function() {
     Log.info('Semo build server running');
